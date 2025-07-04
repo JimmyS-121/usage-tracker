a6049ca0bb3bd25590e43b07a71226d84cef4098
JimmyS-121 <jimmy031201@gmail.com>
2025-07-04 17:34:32 +0800

    Update database.py

--- database.py
+++ database.py
@@ -1,8 +1,9 @@
+import os
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./usage_data.db"
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./usage_data.db")
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False} if "sqlite" in DATABASE_URL else {})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 metadata = MetaData()
