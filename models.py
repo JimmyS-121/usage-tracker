0056e44cb166604b0eca4aad0a5061c525f45151
JimmyS-121 <jimmy031201@gmail.com>
2025-07-04 17:41:42 +0800

    Update models.py

--- models.py
+++ models.py
@@ -1,3 +1,5 @@
+# app/models.py
+
 from sqlalchemy import Column, Integer, String, DateTime, Float
 from sqlalchemy.ext.declarative import declarative_base
 import datetime
@@ -8,7 +10,7 @@ class UsageRecord(Base):
     __tablename__ = "usage_records"
 
     id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(String, index=True)
-    tool_name = Column(String)
-    usage_time = Column(Float)  # e.g., minutes used
-    timestamp = Column(DateTime, default=datetime.datetime.utcnow)
+    user_id = Column(String, index=True, nullable=False)
+    tool_name = Column(String, nullable=False)
+    usage_time = Column(Float, nullable=False)  # e.g., minutes used
+    timestamp = Column(DateTime, default=datetime.datetime.utcnow, nullable=False)
